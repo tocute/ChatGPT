8f200071d0a279553ab4a0266826ab6e94023a07
Bill Chang <billchang@kkstream.com>
2023-05-25 15:11:14 +0800

    Refine UI

--- ChatGPT/Data/Repositories/BaseChatGPTRepository.swift
+++ ChatGPT/Data/Repositories/BaseChatGPTRepository.swift
@@ -8,22 +8,9 @@
 import Foundation
 
 class BaseChatGPTRepository {
+    
     func askChatGPTFor(_ ApiKey: String?, source code: String, language: String) async throws -> Suggestion {
         return Suggestion(result: "")
     }
     
-    func fetchApiKey() -> String? {
-        let currentBundle = Bundle(for: Self.self)
-        
-        guard let apiKeyURL = currentBundle.url(forResource: "openai", withExtension: "environment"),
-              var apiKey = try? String(contentsOf: apiKeyURL, encoding: .utf8)
-        else
-        {
-            return nil
-        }
-        
-        apiKey = apiKey.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        return apiKey
-    }
 }
--- README.md
+++ README.md
@@ -4,3 +4,18 @@ Reference : https://medium.com/globant/chatgpt-integration-in-xcode-how-to-impro
 
 XCode Plugin -> MAC APP
 
+Support Functions:
+    1. Convert JSON Struct
+    2. Code Smells
+    3. Explain Codes
+    4. Test Cases
+    5. Generate Comments
+    6. Others
+
+Support Program Language:
+	1. Swift
+	2. Kotlin
+	3. Javascript
+	4. Python
+	5. Go
+	6. Shell Script
